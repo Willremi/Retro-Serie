c50f7b7769a92ac7ab91b10f072eb0abe8c694fd
Rémi Willebrouck <willremi@gmail.com>
2021-08-07 10:25:52 +0200

    update  fichier bio

--- Views/person/bio.php
+++ Views/person/bio.php
@@ -8,3 +8,8 @@ $notInfo = "Pas d'infos disponibles";
 <h1><?= $bio['name'] ?></h1>
 
 <img src="<?= !$bio['photo'] ? '/img/avatar.png' : 'https://image.tmdb.org/t/p/w154/' . $bio['photo'] ?>" alt="Photo de <?= $bio['name'] ?>" class="rounded">
+
+<div class="offset-md-5 col-md-4">
+    <p>Date de naissance : <?= $bio['dateBirth'] ?></p>
+    <?= ($bio['dateDeath'] !== '01/01/1970') ? "<p>Date de décès : {$bio['dateDeath']}</p>" : "" ?>
+</div>
