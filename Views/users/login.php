bf5b5be31e5e78907b158ffa246e27c4d33f511d
Rémi Willebrouck <willremi@gmail.com>
2021-09-30 12:35:16 +0200

    update barre de menu et ajout de lien mot de passe oublié

--- Views/users/login.php
+++ Views/users/login.php
@@ -1,4 +1,5 @@
 <?php $pageTitle = "Connexion" ?>
 <h1>Connexion</h1>
 <?= $loginForm ?>
-<a href="/users/register">Pas inscrit(e) - S'inscrire</a>
+<a href="/users/register">Pas inscrit(e) - S'inscrire</a><br>
+<a href="#">Mot de passe oublié</a>
